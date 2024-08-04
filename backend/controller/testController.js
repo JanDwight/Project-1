2da31a1b3cc8fcf5036ac00b09a4679d7c33f010
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-08-04 21:35:11 +0800

    CRUD

--- backend/controller/testController.js
+++ backend/controller/testController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler')
+const testModel = require('../models/testModel')
 
 // @desc Get Test
 // @route GET /api/gettest
 // @access Private
 const getTest = asyncHandler (async (req, res) => {
-    res.status(200).json({ message: 'This is a GET test'})
+    const testM = await testModel.find()
+    
+    res.status(200).json(testM)
 })
 
 // @desc Create Test
@@ -15,22 +18,37 @@ const createTest = asyncHandler (async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
+
+    const testM = await testModel.create({
+        text: req.body.text,
+    })
     
-    res.status(200).json({ message: 'This is a SET/CREATE test'})
+    res.status(200).json(testM)
 })
 
 // @desc Update Test
 // @route GET /api/gettest:id
 // @access Private
 const updateTest = asyncHandler (async (req, res) => {
-    res.status(200).json({ message: `This is a UPDATE test ${req.params.id}`})
+    const testM = await testModel.findByIdAndUpdate(req.params.id, req.body, {new: true,})
+
+    res.status(200).json(testM)
 })
 
 // @desc Delete Test
 // @route GET /api/gettest:id
 // @access Private
 const deleteTest = asyncHandler (async (req, res) => {
-    res.status(200).json({ message: `This is a DELETE test ${req.params.id}`})
+    const testM = await testModel.findById(req.params.id)
+
+    if (!testM) {
+        res.status(400)
+        throw new Error('testModel not found')
+    }
+
+    await testM.deleteOne()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 // ???-- Is there a way to put all this and export the class --???
